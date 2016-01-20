3ee84699d9a34f4e33b2e23fc5d6fcb66a672470
xdicary <727119340@qq.com>
2016-01-20 20:15:07 +0800

    modify some name

--- es.h
+++ es.h
@@ -6,9 +6,9 @@
  **
  **/
 
-#include<stdio.h>
-#include<math.h>
-#include<time.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
    
 #define pi 3.14159265
 #define random_max 2147483647   /* max. allowed integer (32-bit) */
@@ -17,57 +17,48 @@
 
 /* particle arrays */ 
 
- float   x[NP_MAX];         /* position */
- float  vx[NP_MAX];         /* velocity */ 
+ float x[NP_MAX];               /* position */
+ float vx[NP_MAX];              /* velocity */ 
 
 /* grid arrays */
 
- float  rhoe[NX_MAX];        /* electron density */
- float  Ex[NX_MAX];           /* electric field */ 
- float  phi[NX_MAX];          /* potential */
- float  a[NX_MAX];           /* laser amplitude */
-
+ float rho[NX_MAX];             /* electron density */
+ float Ex[NX_MAX];              /* electric field */ 
+ float phi[NX_MAX];             /* potential */
 
 /* plasma constants, parameters */
 
- float qe;                /* macro-electron charge */
- float vte;               /* thermal velocity */
- float q_over_me;         /* charge/mass ratio */
- float e_mass;            /* macro-electron mass */
- float xload;             /* plasma length */
- float plasma_start;      /* LH edge of plasma */
- float plasma_end;        /* RH edge of plasma */
- float wall_left;         /* LH particle boundary */
- float wall_right;        /* RH particle boundary */
- float rho0;              /* background (ion) density */
- float grid_length;       /* grid length */
- float omega_p;           /* plasma frequency */
- float x_debye;           /* Debye-length */ 
+ float qe;                      /* macro-electron charge */
+ float vte;                     /* thermal velocity */
+ float q_over_me;               /* charge/mass ratio */
+ float e_mass;                  /* macro-electron mass */
+ float xload;                   /* plasma length */
+ float plasma_start;            /* LH edge of plasma */
+ float plasma_end;              /* RH edge of plasma */
+ float wall_left;               /* LH particle boundary */
+ float wall_right;              /* RH particle boundary */
+ float rho0;                    /* background (ion) density */
+ float grid_length;             /* grid length */
+ float omega_p;                 /* plasma frequency */
+ float lambda_d;                /* Debye-length */ 
 
- int ne, ni;              /* # simulation electrons, ions */
+ int ne, ni;                    /* # simulation electrons, ions */
 
 /* grid variables */
 
- int  ntrun;             /* # timesteps */
- int  itime;             /* current timestep */
- float  dt;              /* timestep */
+ int  nt;                       /* # timesteps */
+ int  i_time;                   /* current timestep */
+ float  dt;                     /* timestep */
 
- int  nx;                /* # grid points */
- float  dx;              /* mesh size */
+ int  nx;                       /* # grid points */
+ float  dx;                     /* mesh size */
 
- int  bc_particle;       /* particle boundary condition switch */
- int  bc_field;          /* field boundary condition */
-
-/* laser parameters */
-
- float  a0;              /* laser amplitude (= vosc/c) */
- float  w0;              /* laser frequency */
- float  trise;           /* laser rise-time */
- int  ilas;              /* laser switch */
+ int  bc_particle;              /* particle boundary condition switch */
+ int  bc_field;                 /* field boundary condition */
 
 /* diagnostic parameters */
  
- int  iout;             /* frequeny of printed output */
- int  igraph;           /* frequeny of graphical output */
- int  ihist;           /* frequeny of time-history output */
+ int  iout;                     /* frequeny of printed output */
+ int  igraph;                   /* frequeny of graphical output */
+ int  ihist;                    /* frequeny of time-history output */
 
